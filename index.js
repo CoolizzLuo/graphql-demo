c05536d378b67d456671ea61a72c5537822e6345
Enzo <50363324+CoolizzLuo@users.noreply.github.com>
2021-11-22 17:51:33 +0800

    add login, signUP

--- index.js
+++ index.js
@@ -1,62 +1,90 @@
 const { ApolloServer, gql } = require('apollo-server');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+// 定義 bcrypt 加密所需 saltRounds 次數
+const SALT_ROUNDS = 2;
+// 定義 jwt 所需 secret (可隨便打)
+const SECRET = 'just_a_random_secret';
 
-// 1. 在假資料中補充朋友資訊
+
+const meId = 1;
 const users = [
-  { id: 1, name: 'Fong', age: 23, friendIds: [2, 3], height: 180, weight: 70.0 },
-  { id: 2, name: 'Kevin', age: 40, friendIds: [1], height: 158, weight: 52.0 },
-  { id: 3, name: 'Mary', age: 18, friendIds: [1], height: 181, weight: 77.0 }
+  {
+    id: 1,
+    email: 'fong@test.com',
+    password: '$2b$04$wcwaquqi5ea1Ho0aKwkZ0e51/RUkg6SGxaumo8fxzILDmcrv4OBIO', // 123456
+    name: 'Fong',
+    age: 23,
+    friendIds: [2, 3]
+  },
+  {
+    id: 2,
+    email: 'kevin@test.com',
+    password: '$2b$04$uy73IdY9HVZrIENuLwZ3k./0azDvlChLyY1ht/73N4YfEZntgChbe', // 123456
+    name: 'Kevin',
+    age: 40,
+    friendIds: [1]
+  },
+  {
+    id: 3,
+    email: 'mary@test.com',
+    password: '$2b$04$UmERaT7uP4hRqmlheiRHbOwGEhskNw05GHYucU73JRf8LgWaqWpTy', // 123456
+    name: 'Mary',
+    age: 18,
+    friendIds: [1]
+  }
 ];
 
 const posts = [
-  { id: 1, authorId: 1, title: "Hello World!", content: "This is my first post.", likeGiverIds: [2] },
-  { id: 2, authorId: 2, title: "Good Night", content: "Have a Nice Dream =)", likeGiverIds: [2, 3] },
-  { id: 3, authorId: 1, title: "I Love U", content: "Here's my second post!", likeGiverIds: [] },
+  {
+    id: 1,
+    authorId: 1,
+    title: 'Hello World',
+    body: 'This is my first post',
+    likeGiverIds: [1, 2],
+    createdAt: '2018-10-22T01:40:14.941Z'
+  },
+  {
+    id: 2,
+    authorId: 2,
+    title: 'Nice Day',
+    body: 'Hello My Friend!',
+    likeGiverIds: [1],
+    createdAt: '2018-10-24T01:40:14.941Z'
+  }
 ];
 
-// The GraphQL schema
-// 2. 在 Schema 添加新 fields
 const typeDefs = gql`
-  # Enum Type 為一種特殊的 Scalar Type ，使用時只能出現裡面有定義到的值且不需要加引號
-  # 進入 JavaSript 中使用時，會轉為 String 格式
-  """
-  高度單位
-  """
-  enum HeightUnit {
-    "公尺"
-    METRE
-    "公分"
-    CENTIMETRE
-    "英尺 (1 英尺 = 30.48 公分)"
-    FOOT
+  type Query {
+    "測試用 Hello World"
+    hello: String
+    "取得目前使用者"
+    me: User
+    "取得所有使用者"
+    users: [User]
+    "依照名字取得特定使用者"
+    user(name: String!): User
+    "取得所有貼文"
+    posts: [Post]
+    "依照 id 取得特定貼文"
+    post(id: ID!): Post
   }
 
-  """
-  重量單位
-  """
-  enum WeightUnit {
-    "公斤"
-    KILOGRAM
-    "公克"
-    GRAM
-    "磅 (1 磅 = 0.45359237 公斤)"
-    POUND
-  }
   """
   使用者
   """
   type User {
     "識別碼"
-    id: ID
+    id: ID!
+    "帳號 email"
+    email: String!
     "名字"
     name: String
     "年齡"
     age: Int
-    "朋友們"
+    "朋友"
     friends: [User]
-    "身高 (預設為 CENTIMETRE)"
-    height(unit: HeightUnit = CENTIMETRE): Float
-    "體重 (預設為 KILOGRAM)"
-    weight(unit: WeightUnit = KILOGRAM): Float
+    "貼文"
     posts: [Post]
   }
 
@@ -71,135 +99,174 @@ const typeDefs = gql`
     "標題"
     title: String
     "內容"
-    content: String
+    body: String
     "按讚者"
     likeGivers: [User]
+    "建立時間 (ISO 格式)"
+    createdAt: String
   }
 
-  type Query {
-    "A simple type for getting started!"
-    hello: String
-    "取得當下使用者"
-    me: User
-    "取得所有使用者"
-    users: [User]
-    "取得特定 user (name 為必填)"
-    user(name: String!): User
+  input UpdateMyInfoInput {
+    name: String
+    age: Int
   }
 
   input AddPostInput {
     title: String!
-    content: String
+    body: String
   }
 
-  # Mutation 定義
+  type Token {
+    token: String!
+  }
+
   type Mutation {
-    "新增貼文"
-    addPost(input: AddPostInput): Post
-    "貼文按讚 (收回讚)"
+    updateMyInfo(input: UpdateMyInfoInput!): User
+    addFriend(userId: ID!): User
+    addPost(input: AddPostInput!): Post
     likePost(postId: ID!): Post
+    "註冊。 email 與 passwrod 必填"
+    signUp(name: String, email: String!, password: String!): User
+    login (email: String!, password: String!): Token
   }
 `;
 
-// Helper Functions
-const findUserById = id => users.find(user => user.id === id);
+// helper functions
+const filterPostsByUserId = userId => posts.filter(post => userId === post.authorId);
+const filterUsersByUserIds = userIds => users.filter(user => userIds.includes(user.id));
+const findUserByUserId = userId => users.find(user => user.id === Number(userId));
 const findUserByName = name => users.find(user => user.name === name);
-const filterPostsByAuthorId = authorId => posts.filter(post => post.authorId === authorId);
-const meId = 1;
-const findPostById = id => posts.find(post => post.id === id);
+const findPostByPostId = postId => posts.find(post => post.id === Number(postId));
+const updateUserInfo = (userId, data) => Object.assign(findUserByUserId(userId), data);
+const addPost = ({ authorId, title, body }) => {
+  posts[posts.length] = {
+    id: posts[posts.length - 1].id + 1,
+    authorId,
+    title,
+    body,
+    likeGiverIds: [],
+    createdAt: new Date().toISOString()
+  };
+};
+const updatePost = (postId, data) => Object.assign(findPostByPostId(postId), data);
+const hash = text => bcrypt.hash(text, SALT_ROUNDS);
+const addUser = ({ name, email, password }) => (
+  users[users.length] = {
+    id: users[users.length - 1].id + 1,
+    name,
+    email,
+    password
+  }
+);
+const createToken = ({ id, email, name }) => jwt.sign({ id, email, name }, SECRET, {
+  expiresIn: '1d'
+});
 
-// 1. 新增 User.posts field Resovler
-// 2. 新增 Post Type Resolver 及底下的 field Resolver
 const resolvers = {
   Query: {
-    hello: () => 'world',
-    me: () => users[0],
-    // 3-1 在 `Query` 裡新增 `users`
+    hello: () => "world",
+    me: (root, args, { me }) => {
+      if (!me) throw new Error('Plz Log In First');
+      return findUserByUserId(me.id)
+    },
     users: () => users,
-    user: (root, args, context) => {
-      // 取出參數。因為 name 為 non-null 故一定會有值。
-      const { name } = args;
-      return users.find(user => user.name === name);
-    }
+    user: (root, { name }, context) => findUserByName(name),
+    posts: () => posts,
+    post: (root, { id }, context) => findPostByPostId(id)
   },
-  Mutation: {
-    addPost: (root, args, context) => {
-      const { input } = args;
-      const { title, content } = input;
-      const newPost = {
-        id: posts.length + 1,
-        authorId: meId,
-        title,
-        content,
-        likeGivers: []
-      };
-      posts.push(newPost);
-      return newPost;
-    },
-    likePost: (root, args, context) => {
-      const { postId } = args;
-      const post = findPostById(postId);
-      if (!post) throw new Error(`Post ${psotId} Not Exists`);
-
-      if (post.likeGiverIds.includes(meId)) {
-        // 如果已經按過讚就收回
-        const index = post.likeGiverIds.findIndex(v => v === userId);
-        post.likeGiverIds.splice(index, 1);
-      } else {
-        // 否則就加入 likeGiverIds 名單
-        post.likeGiverIds.push(meId);
-      }
-      return post;
-    },
-  },
-  // 3-2 新增 `User` 並包含 `friends` 的 field resolver
   User: {
-    // 每個 Field Resolver 都會預設傳入三個參數，
-    // 分別為上一層的資料 (即 user)、參數 (下一節會提到) 以及 context (全域變數)
-    friends: (parent, args, context) => {
-      // 從 user 資料裡提出 friendIds
-      const { friendIds } = parent;
-      // Filter 出所有 id 出現在 friendIds 的 user
-      return users.filter(user => friendIds.includes(user.id));
-    },
-    height: (parent, args) => {
-      const { unit } = args;
-      // 可注意到 Enum type 進到 javascript 就變成了 String 格式
-      // 另外支援 default 值 CENTIMETRE
-      if (!unit || unit === "CENTIMETRE") return parent.height;
-      else if (unit === "METRE") return parent.height / 100;
-      else if (unit === "FOOT") return parent.height / 30.48;
-      throw new Error(`Height unit "${unit}" not supported.`);
-    },
-    // 對應到 Schema 的 User.weight
-    weight: (parent, args, context) => {
-      const { unit } = args;
-      // 支援 default 值 KILOGRAM
-      if (!unit || unit === "KILOGRAM") return parent.weight;
-      else if (unit === "GRAM") return parent.weight * 100;
-      else if (unit === "POUND") return parent.weight / 0.45359237;
-      throw new Error(`Weight unit "${unit}" not supported.`);
-    },
-    posts: (parent, args, context) => {
-      // parent.id 為 userId
-      return filterPostsByAuthorId(parent.id);
-    }
+    posts: (parent, args, context) => filterPostsByUserId(parent.id),
+    friends: (parent, args, context) => filterUsersByUserIds(parent.friendIds || [])
   },
   Post: {
-    // 2-1. parent 為 post 的資料，透過 post.likeGiverIds 連接到 users
-    likeGivers: (parent, args, context) => {
-      return parent.likeGiverIds.map(id => findUserById(id));
+    author: (parent, args, context) => findUserByUserId(parent.authorId),
+    likeGivers: (parent, args, context) => filterUsersByUserIds(parent.likeGiverIds)
+  },
+  Mutation: {
+    updateMyInfo: (parent, { input }, context) => {
+      // 過濾空值
+      const data = ["name", "age"].reduce(
+        (obj, key) => (input[key] ? { ...obj, [key]: input[key] } : obj),
+        {}
+      );
+      return updateUserInfo(meId, data);
     },
-    // 2-2. parent 為 post 的資料，透過 post.author
-    author: (parent, args, context) => {
-      return findUserById(parent.authorId);
+    addFriend: (parent, { userId }, context) => {
+      const me = findUserByUserId(meId);
+      if (me.friendIds.include(userId))
+        throw new Error(`User ${userId} Already Friend.`);
+
+      const friend = findUserByUserId(userId);
+      const newMe = updateUserInfo(meId, {
+        friendIds: me.friendIds.concat(userId)
+      });
+      updateUserInfo(userId, { friendIds: friend.friendIds.concat(meId) });
+
+      return newMe;
+    },
+    addPost: (parent, { input }, context) => {
+      const { title, body } = input;
+      return addPost({ authorId: meId, title, body });
+    },
+    likePost: (parent, { postId }, context) => {
+      const post = findPostByPostId(postId);
+
+      if (!post) throw new Error(`Post ${postId} Not Exists`);
+
+      if (!post.likeGiverIds.includes(postId)) {
+        return updatePost(postId, {
+          likeGiverIds: post.likeGiverIds.concat(meId)
+        });
+      }
+
+      return updatePost(postId, {
+        likeGiverIds: post.likeGiverIds.filter(id => id === meId)
+      });
+    },
+    signUp: async (root, { name, email, password }, context) => {
+      // 1. 檢查不能有重複註冊 email
+      const isUserEmailDuplicate = users.some(user => user.email === email);
+      if (isUserEmailDuplicate) throw new Error('User Email Duplicate');
+
+      // 2. 將 passwrod 加密再存進去。非常重要 !!
+      const hashedPassword = await hash(password, SALT_ROUNDS);
+      // 3. 建立新 user
+      return addUser({ name, email, password: hashedPassword });
+    },
+    login: async (root, { email, password }, context) => {
+      // 1. 透過 email 找到相對應的 user
+      const user = users.find(user => user.email === email);
+      if (!user) throw new Error('Email Account Not Exists');
+
+      // 2. 將傳進來的 password 與資料庫存的 user.password 做比對
+      const passwordIsValid = await bcrypt.compare(password, user.password);
+      if (!passwordIsValid) throw new Error('Wrong Password');
+
+      // 3. 成功則回傳 token
+      return { token: await createToken(user) };
     }
   }
 };
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: async ({ req }) => {
+    // 1. 取出
+    const token = req.headers['x-token'];
+    if (token) {
+      try {
+        // 2. 檢查 token + 取得解析出的資料
+        const me = await jwt.verify(token, SECRET);
+        // 3. 放進 context
+        return { me };
+      } catch (e) {
+        throw new Error('Your session expired. Sign in again.');
+      }
+    }
+    // 如果沒有 token 就回傳空的 context 出去
+    return {};
+  }
 });
 
 server.listen().then(({ url }) => {
