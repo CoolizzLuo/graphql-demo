fc9b15cbaa14e0f24994ff097cd51d6f88c1b76b
Enzo <50363324+CoolizzLuo@users.noreply.github.com>
2021-11-23 18:15:45 +0800

    fix user data

--- models/user.js
+++ models/user.js
@@ -1,27 +1,22 @@
 const users = [
   {
     id: 1,
-    email: 'fong@test.com',
+    email: 'raydu@test.com',
     password: '$2b$04$wcwaquqi5ea1Ho0aKwkZ0e51/RUkg6SGxaumo8fxzILDmcrv4OBIO', // 123456
-    name: 'Fong',
-    age: 23,
-    friendIds: [2, 3]
+    name: '阿低',
   },
   {
     id: 2,
-    email: 'kevin@test.com',
+    email: 'tgop@test.com',
     password: '$2b$04$uy73IdY9HVZrIENuLwZ3k./0azDvlChLyY1ht/73N4YfEZntgChbe', // 123456
-    name: 'Kevin',
-    age: 40,
-    friendIds: [1]
+    name: '那群人',
   },
   {
     id: 3,
-    email: 'mary@test.com',
+    email: 'nwfw@test.com',
     password: '$2b$04$UmERaT7uP4hRqmlheiRHbOwGEhskNw05GHYucU73JRf8LgWaqWpTy', // 123456
-    name: 'Mary',
+    name: '下班不要看',
     age: 18,
-    friendIds: [1]
   }
 ];
 
